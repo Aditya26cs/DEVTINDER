7d7cc45da2bd144983785e6137c12940de9496c0
aditya <amaheshwari819@gmail.com>
2025-06-08 22:41:56 +0530

    no change

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -3,9 +3,9 @@ const requestRoute  = express.Router();
 const {userAuth} = require("../middleware/auth")
 
 requestRoute.post("/sendConnectionRequest" , userAuth ,  (req, res) => {
-     
-    const user = req.user;
-    res.send(user.firstName + "  sends a connection request")
+
+     const user = req.user;
+     res.send(user.firstName + "  sends a connection request")
 })
 
 module.exports = requestRoute;
