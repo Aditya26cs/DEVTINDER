bfb99127e8bc5601be9fd52ba6503eed8b6efce5
Aditya Maheshwari <amaheshwari819@gmail.com>
2025-10-09 21:29:37 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://devtinder-web-seven.vercel.app"],
+    origin: ["http://localhost:5173", "https://devtinder-web-b9w1.vercel.app/"],
     credentials: true
 }));
 
